150343feeb5de5b36f141de205db499a9130c0c1
MohammadTarras <169130680+MohammadTarras@users.noreply.github.com>
2026-06-07 09:01:15 +0300

    Added new req

--- app.py
+++ app.py
@@ -50,7 +50,6 @@ st.set_page_config(
     page_icon="❤️",
     layout="wide",
     initial_sidebar_state="expanded",
-    theme="light"
 )
 
 # ─────────────────────────────────────────────────────────────────────────────
